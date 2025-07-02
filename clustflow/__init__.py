d5f6fb09d9479692798a89710159ccc63c9d6768
gklpcsgn <gklpcsgn94@gmail.com>
2025-07-02 03:23:50 -0400

    import fix

--- clustflow/__init__.py
+++ clustflow/__init__.py
@@ -2,4 +2,5 @@ from clustflow.preprocessing import Encoder, Imputer
 from clustflow.evaluation import compute_metrics, compute_feature_importance, summarize_clusters
 from clustflow.visualization import plot_embeddings, scatter_2d, plot_top_features
 from clustflow.embedding import FlexibleAttentionEmbedding, train_embedding
+from clustflow.utils.seed import set_seed
 
